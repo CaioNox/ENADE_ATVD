da7eb88de8b5abbb2283ee6c53189c6f708365f3
CaioNox <caio.devx@gmail.com>
2026-09-09 21:35:00 -0300

    Adiciona projeto Spring Boot com campos email/nota ENADE e tela de alunos por curso

---
+++ gerenciamento-alunos-crud-springboot-master/src/main/java/br/com/gerenciamento/repository/AlunoRepository.java
@@ -0,0 +1,22 @@
+package br.com.gerenciamento.repository;
+
+import br.com.gerenciamento.model.Aluno;
+import br.com.gerenciamento.enums.Curso;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+
+public interface AlunoRepository extends JpaRepository<Aluno, Long> {
+
+    @Query("SELECT a FROM Aluno a WHERE a.status = 'ATIVO' ")
+    public List<Aluno> findByStatusAtivo();
+
+    @Query("SELECT i FROM Aluno i WHERE i.status = 'INATIVO' ")
+    public List<Aluno> findByStatusInativo();
+
+    public List<Aluno> findByNomeContainingIgnoreCase(String nome);
+
+    public List<Aluno> findByCursoOrderByNomeAsc(Curso curso);
+
+}
